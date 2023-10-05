b9ae8a295d63eac40a01e513b7292f18ac646c34
hryva-ihor <griva.igor422@gmail.com>
2023-10-05 15:57:52 +0300

    hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
@@ -11,12 +11,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Box maxWidth="100%" display="flex" justifyContent="center">
         <Provider store={store}>
           <App />
         </Provider>
       </Box>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
